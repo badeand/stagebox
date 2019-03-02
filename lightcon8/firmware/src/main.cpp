fcaa7d1ea43e34aa80d1fb0a1f0e2cdc9c2e1f20
Andreas Bade <badeand@badeand.com>
2019-03-02 18:18:10 +0100

    neoring24: added esp32 and neopixel

--- lightcon8/firmware/src/main.cpp
+++ lightcon8/firmware/src/main.cpp
@@ -8,8 +8,8 @@
 #include <WebServer.h>
 #include <ESPmDNS.h>
 
-char ssid[] = "Bache";          // your network SSID (name)
-char pass[] = "XXXX";                    // your network password
+char ssid[] = "Andreas's iPhone";          // your network SSID (name)
+char pass[] = "badeerbest";                    // your network password
 
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP Udp;
@@ -35,102 +35,134 @@ void handleIP() {
     server.send(200, "text/html", ipAddress.toString());
 }
 
+const int indicatorPin = 2;
+
+void indicate(int s) {
+    ledcWrite(indicatorPin, s);
+}
+
+void lprint(String s) {
+
+}
+
+void lprintln(String s) {
+
+}
+
 
 void setup() {
 
-    Serial.begin(115200);
-    Serial.println("--- SETUP ---");
+    // Serial.begin(115200);
 
+    lprint("PWN Setup.ch/pin: 2 ..");
+    ledcSetup(indicatorPin, freq, resolution);
+    ledcAttachPin(indicatorPin, indicatorPin);
 
-    /*
+    lprint("PWN Setup.ch/pin: 1 ..");
+    ledcSetup(1, freq, resolution);
+    ledcAttachPin(1, 1);
+    lprint("PWN Setup.ch/pin: 3 ..");
     ledcSetup(3, freq, resolution);
-    ledcSetup(4, freq, resolution);
-    ledcSetup(13, freq, resolution);
-
-    delay(1000);
-
-    Serial.print("..done");
-
-    Serial.print("..attaching pwm channels to the GPIO..");
     ledcAttachPin(3, 3);
+    lprint("PWN Setup.ch/pin: 4 ..");
+    ledcSetup(4, freq, resolution);
     ledcAttachPin(4, 4);
+    lprint("PWN Setup.ch/pin: 13 ..");
+    ledcSetup(13, freq, resolution);
     ledcAttachPin(13, 13);
-    Serial.println("Setup PWM..attach the channel to the GPIO to be controlled..done");
-    Serial.println("Setup PWM..done");
-*/
+    lprintln("done.");
+
+
+    for( int startcnt = 0 ; startcnt < 20 ; startcnt++) {
+        indicate(255);
+        ledcWrite(1, 255);
+        ledcWrite(3, 255);
+        ledcWrite(4, 255);
+        ledcWrite(13, 255);
+        delay(35);
+        indicate(0);
+        ledcWrite(1, 0);
+        ledcWrite(3, 0);
+        ledcWrite(4, 0);
+        ledcWrite(13, 0);
+        delay(35);
+    }
+    indicate(20);
+
+    lprintln("--- SETUP ---");
 
    // Connect to WiFi network
-   Serial.print("Connecting to ");
-   Serial.println(ssid);
+   lprint("Connecting to ");
+   lprintln(ssid);
    WiFi.begin(ssid, pass);
 
    while (WiFi.status() != WL_CONNECTED) {
-       delay(500);
-       Serial.print(".");
+       indicate(255);
+       delay(255);
+       indicate(0);
+       delay(255);
+       lprint(".");
    }
-   Serial.println("");
+   lprintln("");
 
-   Serial.println("WiFi connected");
-   Serial.println("IP address: ");
-   Serial.println(WiFi.localIP());
+   lprintln("WiFi connected");
+   lprintln("IP address: ");
+   lprintln(""+ WiFi.localIP());
    ipAddress = WiFi.localIP();
 
-   Serial.print("Starting UDP...");
-   Udp.begin(localPort);
-   Serial.println("Done");
+   indicate(64);
 
-   Serial.print("Starting MDNS...");
+   lprint("Starting UDP...");
+   Udp.begin(localPort);
+   lprintln("Done");
+
+   indicate(255);
+
+   lprint("Starting MDNS...");
    if (MDNS.begin("lightcon8_1")) {
-       Serial.println("MDNS responder started");
+       lprintln("MDNS responder started");
    }
-   Serial.println("Done");
+   lprintln("Done");
+
+   indicate(128);
 
    server.on("/", handleRoot);
    server.on("/ip", handleIP);
    server.begin();
-   Serial.println("HTTP server started");
+   lprintln("HTTP server started");
 
 
-    Serial.println("Setting up PWM");
+    lprintln("Setting up PWM");
 
 
-    Serial.print("PWN Setup.ch/pin: 1 ..");
-    ledcSetup(1, freq, resolution);
-    ledcAttachPin(1, 1);
-    Serial.print("PWN Setup.ch/pin: 3 ..");
-    ledcSetup(3, freq, resolution);
-    ledcAttachPin(3, 3);
-    Serial.print("PWN Setup.ch/pin: 4 ..");
-    ledcSetup(4, freq, resolution);
-    ledcAttachPin(4, 4);
-    Serial.print("PWN Setup.ch/pin: 13 ..");
-    ledcSetup(13, freq, resolution);
-    ledcAttachPin(13, 13);
-    Serial.println("done.");
-
-    Serial.println("--- SETUP DONE ---");
+    lprintln("--- SETUP DONE ---");
 
 
+    indicate(20);
 }
 
 void led(OSCMessage &msg) {
+
     ledPin = msg.getInt(0);
     ledState = msg.getInt(1);
-//    digitalWrite(ledPin, ledState);
     ledcWrite(ledPin, ledState);
-    Serial.print("pin:" );
-    Serial.print( ledPin );
-    Serial.print(" state: ");
-    Serial.println(ledState);
-
+    // lprint("pin:" );
+    // lprint( ledPin );
+    // lprint(" state: ");
+    // lprintln(ledState);
 }
 
+long timestamp = millis();
+
 void loop() {
+
+    // lprint(".");
     server.handleClient();
     OSCMessage msg;
     int size = Udp.parsePacket();
 
     if (size > 0) {
+        timestamp = millis() + 250;
         while (size--) {
             msg.fill(Udp.read());
         }
@@ -138,9 +170,15 @@ void loop() {
             msg.dispatch("/led", led);
         } else {
             error = msg.getError();
-            Serial.print("error: ");
-            Serial.println(error);
+            lprint("error: ");
+            lprintln(""+error);
         }
+    }
+
+    if( millis() < timestamp ) {
+        indicate(255);
+    } else {
+        indicate(10);
     }
 
 }
---
+++ neoring24/firmware/src/main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Andreas Rosnes Bade on 2019-03-02.
+//
+
+#include "main.h"
